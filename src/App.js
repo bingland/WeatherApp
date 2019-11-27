eb60473a0d93626ced3769392fc66aca16367b53
Brayden England <bray.patriot@gmail.com>
2019-11-26 17:35:28 -0700

    Added Weather Component with Fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
 import Canvas from './components/Canvas'
+import Weather from './components/Weather'
 
 function App() {
+
+    fetch("https://api.openweathermap.org/data/2.5/weather?callback=test&id=2172797&units=%2522metric%2522%20or%20%2522imperial%2522&q=London%252Cuk&APPID=58ee50dbbe686e7219635112c7593425", {
+    "method": "GET"
+  })
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
   return (
     <div className="App">
-      hello
       <Canvas />
+      <Weather />
     </div>
   );
 }
--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,18 +1,26 @@
 import React from 'react';
 
 class Canvas extends React.Component {
-    componentDidMount() {
-      const canvas = this.refs.canvas
-      const ctx = canvas.getContext("2d")
-    }
-
-    render() {
-        const canvasStyle = {
-            border: "1px solid black"
-        }
-      return(
-        <canvas ref="canvas" width={640} height={425} style={canvasStyle} />
-      )
-    }
+  componentDidMount() {
+    const canvas = this.refs.canvas
+    const ctx = canvas.getContext("2d")
   }
-  export default Canvas
+
+  render() {
+    return(
+      <div>
+        <canvas ref="canvas" width={window.innerWidth} height={window.innerHeight} style={canvasStyle} />
+      </div>
+    )
+  }
+}
+
+const canvasStyle = {
+  border: "1px solid black",
+  display: 'none',
+  padding: '0',
+  margin: '0'
+}
+
+
+export default Canvas
